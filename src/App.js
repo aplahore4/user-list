7b68cf06caa1097fa602bb73cd40a01a2aff8684
ahmadPervaviz <aplahore4@gmail.com>
2022-09-02 22:15:53 +0500

    hello world 101 added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
 
 function App() {
-  return <div className='App'>Hello world</div>
+  return (
+    <div className='App'>
+      <div className='App'>Hello world</div>
+      <div className='App'>Hello world 101</div>
+    </div>
+  )
 }
 
 export default App
